0a9a6d3b87df22a70c5102fb4ed681ced092fb02
LTabhishek <abhi8010bhandari@gmail.com>
2026-02-28 17:36:11 +0530

    learning how to decalre a variable

--- udemy-practice/01-variable.js
+++ udemy-practice/01-variable.js
@@ -1,3 +1,3 @@
-// Define a variable called myLuckyNumber and set it equal to 37
+// Define a variable called myLuckyNumber and set it equal to 37 (let keyword)
 let myLuckyNumber = 37;
 let octopusLimbs = 8;
---
+++ udemy-practice/02-const.js
@@ -0,0 +1,3 @@
+// Define a constant called boilingPointC,boilingPointF and set it equal to 100 and 212 respectively (const keyword)
+const boilingPointC = 100;
+const boilingPointF = 212;
