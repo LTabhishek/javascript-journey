3e927f5d1240edd7ccd5d26e1ef2151330000140
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-03 17:54:32 +0530

    understanding slice and replace methods using example

---
+++ udemy-practice/05-slice,replace.js
@@ -0,0 +1,9 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+
+let facialHair = word.slice(5).replace("o", "e");
+console.log(facialHair);
+
+
