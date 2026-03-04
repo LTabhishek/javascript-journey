c4da82e13ed6472e49707117865f4ff70947b6c3
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-04 17:39:06 +0530

    learning conditional statements

--- conditional/conditional.js
+++ conditional/conditional.js
@@ -1,11 +1,15 @@
 console.log("Conditional Statements in JS");
 
 // if statement
-console.log("Before Statement");
-
+// 1.age example
 let age = 18;
 if (age >= 18) {
     console.log("you can vote");
 }
 
-console.log("After Statement");
+// 2.a random number is greater than or not
+
+let num = Math.floor(Math.random() * 10) + 1;
+if (num >= 5) {
+    console.log(num + " is greater than or equal to 5");
+}
