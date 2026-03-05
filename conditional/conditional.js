e055621c7cffe01a927a6d5f775aea693c79d11a
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-05 19:48:32 +0530

    learning nesting conditions

--- conditional/conditional.js
+++ conditional/conditional.js
@@ -1,34 +1,49 @@
-console.log("Conditional Statements in JS");
+// console.log("Conditional Statements in JS");
 
-// if statement
-// 1.age example
-let age = 18;
-if (age >= 18) {
-    console.log("you can vote");
-}
+// // if statement
+// // 1.age example
+// let age = 18;
+// if (age >= 18) {
+//     console.log("you can vote");
+// }
 
-// 2.a random number is greater than or not
+// // 2.a random number is greater than or not
 
-let num = Math.floor(Math.random() * 10) + 1;
-if (num >= 5) {
-    console.log(num + " is greater than or equal to 5");
-}
+// let num = Math.floor(Math.random() * 10) + 1;
+// if (num >= 5) {
+//     console.log(num + " is greater than or equal to 5");
+// }
 
-// if else statement
-// 1.age example
-let age1 = 18;
-if (age1 >= 18) {
-    console.log("you can vote");
+// // if else statement
+// // 1.age example
+// let age1 = 18;
+// if (age1 >= 18) {
+//     console.log("you can vote");
+// }
+// else {
+//     console.log("you cannot vote");
+// }
+// // // 2.rating a actor
+
+// let rating = 3;
+// if (rating >= 3) {
+//     console.log("You are a superstar");
+// }
+// else {
+//     console.log("You are a good actor");
+// }
+
+const password = prompt("Enter your password");
+// password should be at least 6 characters
+if (password.length >= 6) {
+    // password should not contain spaces
+    if (password.indexOf(' ') === -1) {
+        console.log("Password is valid");
+    }
+    else {
+        console.log("Password does not contain spaces");
+    }
 }
 else {
-    console.log("you cannot vote");
-}
-// // 2.rating a actor
-
-let rating = 3;
-if (rating >= 3) {
-    console.log("You are a superstar");
-}
-else {
-    console.log("You are a good actor");
+    console.log("Password is too short");
 }
