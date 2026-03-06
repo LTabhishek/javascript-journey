8ddb37c080f229fe316270ffdb58369a6847a688
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-06 13:20:48 +0530

    logical operators

--- logical operators/logical.js
+++ logical operators/logical.js
@@ -9,3 +9,12 @@ if (age >= 18 && userID) {
 else {
     console.log("you cannot enter")
 }
+
+// password should be at least 6 characters and should not contain spaces
+const password = prompt("Enter your password");
+if (password.length >= 6 && password.indexOf(' ') === -1) {
+    console.log("Password is valid");
+}
+else {
+    console.log("Password is invalid");
+}
