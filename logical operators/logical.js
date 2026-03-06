09d78e96b1a8eb44a7d541252f8eb32d4df7e509
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-06 20:02:09 +0530

    Switch statement

--- logical operators/logical.js
+++ logical operators/logical.js
@@ -20,16 +20,38 @@
 // }
 
 // Or operator (||) - at least one condition must be true
-let age = prompt("Enter your age");
-if (age >= 0 && age < 10 || age > 65) {
-    console.log("free");
-}
-else if (age >= 5 && age < 10) {
-    console.log("child discount");
-}
-else if (age >= 10 && age <= 65) {
-    console.log("full price");
-}
-else {
-    console.log("invalid age");
+// let age = prompt("Enter your age");
+// if (age >= 0 && age < 10 || age > 65) {
+//     console.log("free");
+// }
+// else if (age >= 5 && age < 10) {
+//     console.log("child discount");
+// }
+// else if (age >= 10 && age <= 65) {
+//     console.log("full price");
+// }
+// else {
+//     console.log("invalid age");
+// }
+
+//switch statement  
+const day = Number(prompt("Enter a day of the week (1-7)"));
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    default:
+        console.log("Weekend");
 }
