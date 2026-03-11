6f1304c584cf7b1694f7319ad7304af24c28ac1d
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-11 18:17:43 +0530

    loops in js

--- loops in js/loops.js
+++ loops in js/loops.js
@@ -1,3 +1,15 @@
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
+//for loops in java script
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// //to print even numbers from 0 to 20
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// count down form 20 to 0
+// for (let i = 20; i >= 0; i--) {
+//     console.log(i);
+// }
