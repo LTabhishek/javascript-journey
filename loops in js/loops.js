4654160c7bff5f26b72cbe491f0002d788e10942
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-13 12:45:49 +0530

    loops in js

--- loops in js/loops.js
+++ loops in js/loops.js
@@ -13,3 +13,11 @@
 // for (let i = 20; i >= 0; i--) {
 //     console.log(i);
 // }
+
+// //nested loops
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`i is : ${i}`)
+//     for (let j = 1; j < 4; j++) {
+//         console.log(`   j is : ${j}`)
+//     }
+// }
