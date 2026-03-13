acc66d2e230da485aa99d48c4ba728131de2f68f
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-13 20:29:45 +0530

    practice of loops

--- loops in js/loops.js
+++ loops in js/loops.js
@@ -21,3 +21,33 @@
 //         console.log(`   j is : ${j}`)
 //     }
 // }
+let num = 7;
+for (let i = 1; i <= 1; i++) {
+    for (j = 1; j <= 10; j++)
+        console.log(`${num * j}`)
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+
+    for (let i = 2; i <= 20; i += 2) {
+        console.log(i)
+    }
+}
+for (let i = 10; i >= 1; i--) {
+    console.log(i)
+}
+
+let fruits = ["apple", "banana", "mango", "orange"]
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+let people = ['scooby', 'velma', 'daphane', 'shaggy', 'fred']
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
+let num = 7;
+for (let j = 1; j <= 10; j++) {
+    console.log(`${num}*${j} :${num * j}`)
+}
