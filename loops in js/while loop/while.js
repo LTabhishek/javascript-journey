036001405336b609f9f4310321c3c9b23594342a
LTabhishek <abhi8010bhandari@gmail.com>
2026-03-18 12:25:51 +0530

    while loop

---
+++ loops in js/while loop/while.js
@@ -0,0 +1,29 @@
+// // // While Loop
+// // let i = 0;
+// // while (i < 5) {
+// //     console.log(i);
+// //     i++;
+// // }
+
+// // // to print even numbers from 0 to 20
+// // let j = 0;
+// // while (j <= 20) {
+// //     console.log(j);
+// //     j += 2;
+// // }
+
+// // // count down form 20 to 0  .
+
+// // let a = 20;
+// // while (a >= 0) {
+// //     console.log(a);
+// //     a--;
+// // }
+
+// //guessing a secret code
+// let secret = "Zoro";
+// let guess = prompt("Enter the secret code");
+// while (guess !== secret) {
+//     guess = prompt("Wrong! Enter the secret code");
+// }
+// console.log("Correct! The secret code is " + secret);
